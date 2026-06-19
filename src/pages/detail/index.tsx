3af53fa4ea12ec7c2405542051f63ff058ce5c22
M373746 <j.beskow@sistran.com.br>
2026-06-19 15:38:00 -0300

    react project of crypto current first commit

---
+++ src/pages/detail/index.tsx
@@ -0,0 +1,10 @@
+export function Detail() {
+
+    return (
+      <div>
+        <h1>Página Detalhes da moeda</h1>
+      </div>
+    )
+  }
+
+  
---
+++ src/pages/notfound/index.tsx
@@ -0,0 +1,10 @@
+export function NotFound() {
+
+    return (
+      <div>
+        <h1>Erro 404, essa página não existe</h1>
+      </div>
+    )
+  }
+
+  
