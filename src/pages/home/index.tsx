92563c37eaab6924f5bad25c4e6bba5fa2107763
M373746 <j.beskow@sistran.com.br>
2026-06-20 17:58:33 -0300

    adding interaction with external api

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,95 @@
-import { BsSearch } from 'react-icons/bs'
-import styles from './home.module.css'
-import { Link } from 'react-router-dom'
+import { useEffect, useState, type FormEvent } from 'react';
+import { BsSearch } from 'react-icons/bs';
+import { Link, useNavigate } from 'react-router-dom';
+import styles from './home.module.css';
+
+
+interface CoinProps {
+  id: string;
+  name: string;
+  symbol: string;
+  priceUsd: string;
+  vwap24Hr: string;
+  changePercent24Hr: string;
+  rank: string;
+  supply: string;
+  maxSupply: string;
+  marketCapUsd: string;
+  volumeUsd24Hr: string;
+  explorer: string;
+}
+
+interface DataProp {
+  data: CoinProps[];
+}
+
+
+
+
 export function Home() {
 
-    return (
-      <main className={styles.container}>
-        <form className={styles.form}>
-          <input
-            type="text"
-            placeholder="Digite o nome da moeda..."/>
-            <button type="submit">
-              <BsSearch size={30} color="#FFF"></BsSearch>
-            </button>
-        </form>
+  const [input, setInput] = useState("");
+  const [coins, setCoins] = useState<CoinProps[]>([]);
+  const navigate = useNavigate();
+
+  
+
+  async function getData() {
+    const baseUrl = import.meta.env.VITE_API_URL;
+    const token = import.meta.env.VITE_API_TOKEN;
+    const url = `${baseUrl}?limit=10`;
+
+    const options = {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    };
+
+    fetch(url, options)
+      .then(response => response.json())
+      .then((data: DataProp) => {
+        const coinsData = data.data;
+        const price = Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD"
+        })
+        const formatedResult = coinsData.map((item) => {
+          const formated = {
+            ...item,
+            formatedPrice: price.format(Number(item.priceUsd)),
+            formatedMarket: price.format(Number(item.marketCapUsd))
+          }
+          return formated;
+        })
+        console.log(formatedResult);
+      })
+  }
+
+  useEffect(() => { getData() }, [])
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    if (input === "") return;
+    const link = `/detail/${input}`;
+    navigate(link);
+  }
+
+  return (
+    <main className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Digite o nome da moeda..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit">
+          <BsSearch size={30} color="#FFF"></BsSearch>
+        </button>
+      </form>
 
       <table>
         <thead>
@@ -49,12 +126,11 @@ export function Home() {
         </tbody>
       </table>
 
+      <button>
+        Carregar mais...
+      </button>
 
+    </main>
+  )
+}
 
-
-
-      </main>
-    )
-  }
-
-  
